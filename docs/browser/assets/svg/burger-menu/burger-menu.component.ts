4a4d3501cc68ce7efc66254ecd6c494497a88b94
devSamuel <samueldflima@proton.me>
2024-08-20 00:58:23 -0300

    project: deploy

--- docs/browser/assets/svg/burger-menu/burger-menu.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'burger-menu-svg',
-  standalone: true,
-  templateUrl: './burger-menu.svg',
-})
-export class BurgerMenuComponent {}
